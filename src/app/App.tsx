22d06bc9dd479064534aa564c9bd4bc78113a69d
igor_malykhin <malykhinig@gmail.com>
2025-07-24 10:17:03 +0700

    chore:ErrorBoundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { AppRouter } from './providers/router';
 import { classNames } from '../shared/config/lib/classNames/classNames';
 import './styles/index.scss';
 
 export function App() {
     const { theme } = useTheme();
+
+    useEffect(() => {
+        if (Math.random() < 0.5) {
+            throw new Error('error');
+        }
+    }, []);
 
     return (
         <div className={classNames('app', {}, [theme])}>
