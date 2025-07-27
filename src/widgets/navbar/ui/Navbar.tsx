740869d5e9658a14acdf1d3f012200a9f3f6c660
igor_malykhin <malykhinig@gmail.com>
2025-07-27 14:29:32 +0700

    fix:navbar link translation

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,35 +1,37 @@
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { classNames } from 'shared/config/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className="links">
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
 
-            <AppLink
-                theme={AppLinkTheme.SECONDARY}
-                to="/"
-                className={cls.mainLink}
-            >
-                {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className="links">
+
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/"
+                    className={cls.mainLink}
+                >
+                    {t('Главная')}
+                </AppLink>
+                <AppLink
+                    theme={AppLinkTheme.PRIMARY}
+                    to="/about"
+                    className={cls.mainLink}
+                >
+                    {
                     // eslint-disable-next-line i18next/no-literal-string
-                    'Главная'
-                }
-            </AppLink>
-            <AppLink
-                theme={AppLinkTheme.PRIMARY}
-                to="/about"
-                className={cls.mainLink}
-            >
-                {
-                    // eslint-disable-next-line i18next/no-literal-string
-                    'О нас'
-                }
-            </AppLink>
+                        'О нас'
+                    }
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
