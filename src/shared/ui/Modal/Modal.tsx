36d5556280cced01dc20e83c4437bbbfb1a8745d
gibidon <malykhinig@gmail.com>
2025-08-13 23:36:26 +0700

    fix:modal references

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -80,7 +80,7 @@ export const Modal = (props: ModalProps) => {
     }
 
     return (
-        <Portal element={document.body}>
+        <Portal element={document.getElementById('root')!}>
             <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div
--- src/shared/ui/Portal/Portal.tsx
+++ src/shared/ui/Portal/Portal.tsx
@@ -1,15 +1,32 @@
+// import { ReactNode } from 'react';
+// import { createPortal } from 'react-dom';
+
+// interface PortalProps {
+//     children: ReactNode
+//     element?: HTMLElement
+// }
+
+// export const Portal = (props: PortalProps) => {
+//     const { children, element = document.body } = props;
+
+//     if (!element) return null;
+
+//     return createPortal(children, element);
+// };
+
 import { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
 interface PortalProps {
-    children: ReactNode
-    element?: HTMLElement
+    children: ReactNode;
+    element?: HTMLElement;
 }
 
 export const Portal = (props: PortalProps) => {
-    const { children, element = document.body } = props;
-
-    if (!element) return null;
+    const {
+        children,
+        element = document.body,
+    } = props;
 
     return createPortal(children, element);
 };
